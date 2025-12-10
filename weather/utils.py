f73146480ca36c7003744d5a0dd175e881613f34
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-12-11 01:13:55 +0300

    feat: city name to coordinates via Nominatim

---
+++ weather/utils.py
@@ -0,0 +1,14 @@
+import requests 
+ 
+def city_to_coords(city: str): 
+    url = "https://nominatim.openstreetmap.org/search" 
+    params = {"q": city, "format": "json", "limit": 1} 
+    headers = {"User-Agent": "weather-cli-student/1.0"} 
+    try: 
+        r = requests.get(url, params=params, headers=headers, timeout=10) 
+        r.raise_for_status() 
+        data = r.json() 
+        if not data: raise ValueError 
+        return float(data[0]["lat"]), float(data[0]["lon"]) 
+    except: 
+        raise RuntimeError(f"Город '{city}' не найден") 
